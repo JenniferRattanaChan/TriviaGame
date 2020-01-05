82af3cf9f490767d39e49c76bff8131cab535d95
Jennifer Chan <jenniferchan2267@yahoo.com>
2020-01-05 08:01:52 -0800

    add questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+var questions = [{
+    question: "What is Mr. Krabs' full name? ",
+    answerList: ["Edward Krabs","Eugene Krabs","Ernest Krabs","Keith Krabs"],
+    answer: 1
+},{
+    question: "What does SpongeBob SquarePants live in?",
+    answerList: ["A flat","A boat", "A pineapple","A Shoe"],
+    answer: 2
+},{
+    question: "How many fingers and thumbs does SpongeBob SquarePants have?",
+    answerList: ["4", "6", "8", "10"],
+    answer: 2
+},{
+    question: "What are SpongeBob SquarePants' parents called?",
+    answerList: ["Kevin and Mary","Henry and Joanne","Thomas and Linda","Harold and Margaret"],
+    answer: 3
+},{
+    question: "What is Patrick's last name?",
+    answerList: ["Star", "Fartington", "Shelley", "Coddington"],
+    answer: 0
+},{
+    question: "Who is SpongeBob's friend from Texas?",
+    answerList: ["Squirrel Nutkin","Sanday Cheeks","Hazel Nutt", "Jane Bushytail"],
+    answer: 1
+},{
+    question: "What symbol is on SpongeBob SquarePants' white Krusty Krab hat?",
+    answerList: ["A small blue anchor", "A smiley face", "A packet of chips", "A krabby patty"],
+    answer: 0
+},{
+    question: "Where does Squidward Tentacles live?",
+    answerList: ["Behind a shipwreck","In a cave","A submarine","Between SpongeBob and Patrick's House"],
+    answer: 3
+},{
+    question: "What is Gary the Snail's full name?",
+    answerList: ["Garth Wilson Jr.", "Garfield Wilson Jr.", "Garold Wilson Jr.", "Garrington Wilson Jr."],
+    answer: 2
+},{
+    question: "What is Mrs. Puff's first name?",
+    answerList: ["Penny", "Poppy", "Sugar", "Pamela"],
+    answer: 1
+}];
